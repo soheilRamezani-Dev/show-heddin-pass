79563969784f4a6c5113d07d0c083d0436ffe997
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-09-09 11:43:26 +0430

    style compeleted

--- src/components/form.js
+++ src/components/form.js
@@ -1,14 +1,47 @@
 import './form.css';
+import { useFormik} from 'formik';
+import { useState } from 'react';
+import * as yup from 'yup'
+
 const Form = ()=>{
+
+    const [isshow , setisshow] =useState(false)
+
+    const formik = useFormik({
+        initialValues:{
+            password:''
+        },
+        validateYupSchema: yup.object({
+            password: yup.string().min(5).required()
+        }),
+        onSubmit: values =>{
+            console.log(values);
+        }
+
+    });
+
+    function showPass(){
+        if(isshow === false){
+            setisshow(true);
+         }else{
+            setisshow(false);
+         }
+        
+    }
+
     return <>
         <form>
-            <div className="form-group">
+            <div className="form-group justify-content-center d-flex">
                 <label htmlFor="password">
-                    password:
-                    <input type="password" name="password" placeholder="password" />
-                    <i className="show-password-icon award-icon fa-solid fa-lg"></i>
-                </label>
-                
+                    <input onBlur={formik.handleBlur} 
+                    value={formik.values.password} 
+                    onChange={formik.handleChange} 
+                    className='form-control' 
+                    type={(isshow)? 'text' : 'password'} 
+                    name="password" 
+                    placeholder="password"/>
+                    <i className="show-password-icon award-icon fa-solid fa-lg" onClick={showPass}></i>
+                </label>           
             </div>
         </form>
     </>
